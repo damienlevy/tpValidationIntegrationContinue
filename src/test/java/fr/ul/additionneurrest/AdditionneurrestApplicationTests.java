85db04c95bf14d87c10091e41793590e056db06e
Damien LEVY <damien.levy4@etu.univ-lorraine.fr>
2020-12-03 17:31:32 +0100

    ajout travis.yml

--- src/test/java/fr/ul/additionneurrest/AdditionneurrestApplicationTests.java
+++ src/test/java/fr/ul/additionneurrest/AdditionneurrestApplicationTests.java
@@ -2,13 +2,23 @@ package fr.ul.additionneurrest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 
 class AdditionneurrestApplicationTests {
 
     @Test
     void change_base() {
+        given().param("base",5).when().post("/adder/changeBase");
+        when().get("/adder/current").then().assertThat().body(equalTo("5"));
+    }
 
+    @Test
+    void rand(){
+        //when().get("/adder/random").then().assertThat().body()
     }
 
 }
